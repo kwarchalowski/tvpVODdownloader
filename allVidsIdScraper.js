75212bc891e136e2a5f1fc34b75b8ec8c08ea1fe
kwarchalowski <karol@warchalowski.pl>
2021-07-17 22:28:59 +0200

    A lot of broken things (async...)

--- allVidsIdScraper.js
+++ allVidsIdScraper.js
@@ -1,8 +1,16 @@
 const rp = require('request-promise');
 const async = require('async');
 const $ = require('cheerio');
-const url = 'https://vod.tvp.pl/website/ojciec-mateusz,1667840/video';
 
+module.exports = {
+	parse: function() {
+		parse();
+	}
+}
+
+
+//const url = 'https://vod.tvp.pl/website/ojciec-mateusz,1667840/video';
+const url = 'https://vod.tvp.pl/website/egzamin-z-zycia,1667578/video';
 const vidUrls = [];
 
 var basePageUrl = url + "?order=oldest&page="
@@ -12,49 +20,51 @@ var vidCount = 0;
 
 // get pages count 
 const parse = async _ => {
-await rp(url)
-  .then(async function(html){
+	await rp(url)
+		.then(async function (html) {
 
-    //success!
-    pagesCount = $('.pagination > li.lastItem > a', html)[0].attribs.href.split('=')[1];
-    //console.log($('.pagination > li.lastItem > a', html)[0].attribs.href.split('=')[1]);
-    //for (let i = 0; i < 50; i++) {
- 	//   vidUrls.push($('.strefa-abo__item-link', html)[i].attribs.href);
-	//}
-	console.log("Pages count: " + pagesCount);
+			//success!
+			pagesCount = $('.pagination > li.lastItem > a', html)[0].attribs.href.split('=')[1];
+			//console.log($('.pagination > li.lastItem > a', html)[0].attribs.href.split('=')[1]);
+			//for (let i = 0; i < 50; i++) {
+			//   vidUrls.push($('.strefa-abo__item-link', html)[i].attribs.href);
+			//}
+			console.log("Pages count: " + pagesCount);
 
-	// get all video urls
-		for (let i = 1; i <= pagesCount; i++) {
-		await rp(basePageUrl + i)
-		  .then(async function(html){
-		
-		
-		    //success!
-		    vidCount = $('.strefa-abo__item-link', html).length;
-		    //console.log($('.pagination > li.lastItem > a', html)[0].attribs.href.split('=')[1]);
-		    for (let i = 0; i < vidCount; i++) {
-		 	   vidUrls.push($('.strefa-abo__item-link', html)[i].attribs.href.substring(7));
+			// get all video urls
+			for (let i = 1; i <= pagesCount; i++) {
+				await rp(basePageUrl + i)
+					.then(async function (html) {
+
+
+						//success!
+						vidCount = $('.strefa-abo__item-link', html).length;
+						//console.log($('.pagination > li.lastItem > a', html)[0].attribs.href.split('=')[1]);
+						for (let i = 0; i < vidCount; i++) {
+							vidUrls.push($('.strefa-abo__item-link', html)[i].attribs.href.substring(7));
+						}
+						console.log("Vids on page " + i + ": " + vidCount);
+					})
+					.catch(function (err) {
+						console.error(err);
+					});
+
 			}
-			console.log("Vids on page " + i + ": " + vidCount);
-			})
-		  	.catch(function(err){
-		    //handle error
-		  	});
-		  	
-		}
 
-})
-.catch(function(err){
-  //handle error
-});
-console.log("All videos count: " + vidUrls.length);
-console.log(vidUrls);
-return vidUrls;
+		})
+		.catch(function (err) {
+			console.error(err);
+		});
+	console.log("All videos count: " + vidUrls.length);
+	//console.log(vidUrls);
+	return vidUrls;
 }
 
 
 
-parse();
+//parse();
+
+
 
  	   //vidUrls.push($('.strefa-abo__item-link', html)[i].attribs.href);
 
--- tvpdown.js
+++ tvpdown.js
@@ -7,6 +7,8 @@ const fetch = require("node-fetch");
 // askQuestion (prob. about bitrate)
 const prompt = require('prompt-sync')();
 
+const allVids = require('./allVidsIdScraper');
+
 // passedArgs -
 // 0 - site URL
 // 1 - downloads folder name (dirName) at place
@@ -26,6 +28,7 @@ var dirName = passedArgs[1];
 
 //check if directory exists, create one if not
 const fs = require('fs');
+const { timeout } = require("async");
 const dir = './' + dirName;
 
 if (fs.existsSync(dirName)) {
@@ -51,7 +54,7 @@ var APIresponse;
 
 fetch(downloadURL)
 	.then(function (response) {
-		response.text().then(function (text) {
+		response.text().then(async function (text) {
 			APIresponse = text;
 			//console.log(APIresponse);
 
@@ -65,88 +68,102 @@ fetch(downloadURL)
 		});
 	});
 
-function done() {
-	console.log("\x1b[32m%s\x1b[0m", '\nSuccesfully got data from TVP API!\n');
+async function done() {
 
-	var vidTitle = escape(APIresponse.match(/"title":.*/)[0].slice(10, -2));  // returning video (program) name
-	console.log("Video title: " + "\x1b[33m%s\x1b[0m", vidTitle);
+	try {
 
-	var vidSubtitle = escape(APIresponse.match(/"subtitle":.*/)[0].slice(13, -2).replace("\\", "")); // returning video (subtitle) name
-	console.log("Video subtitle: " + "\x1b[33m%s\x1b[0m", vidSubtitle);
+		// TODO: it's BROKEN HEREEEEEEEEEEEEEEEEEE
+		//[urlsList] = await allVids.parse();
+		let urlsList = await allVids.parse();
+
+		console.log('Urls list: ' + urlsList);
+
+		console.log("\x1b[32m%s\x1b[0m", '\nSuccesfully got data from TVP API!\n');
+
+		var vidTitle = escape(APIresponse.match(/"title":.*/)[0].slice(10, -2));  // returning video (program) name
+		console.log("Video title: " + "\x1b[33m%s\x1b[0m", vidTitle);
+
+		var vidSubtitle = escape(APIresponse.match(/"subtitle":.*/)[0].slice(13, -2).replace("\\", "")); // returning video (subtitle) name
+		console.log("Video subtitle: " + "\x1b[33m%s\x1b[0m", vidSubtitle);
 
 
-	var vidURLs = APIresponse.match(/"url":.*(mp4|m3u8)/g); // all video URLs
-	var vidBitrates = APIresponse.match(/"bitrate":.*/g); // videos bitrates
+		var vidURLs = APIresponse.match(/"url":.*(mp4|m3u8)/g); // all video URLs
+		var vidBitrates = APIresponse.match(/"bitrate":.*/g); // videos bitrates
 
 
-	// getting maxBitate video's index from matches
-	//console.log("Available bitrates:\n");
-	for (var i = 0; i < vidBitrates.length; i++) {
-		vidBitrates[i] = parseInt(vidBitrates[i].slice(11, -1));
-		//console.log(i+1 + " - " + vidBitrates[i]);
+		// getting maxBitate video's index from matches
+		//console.log("Available bitrates:\n");
+		for (var i = 0; i < vidBitrates.length; i++) {
+			vidBitrates[i] = parseInt(vidBitrates[i].slice(11, -1));
+			//console.log(i+1 + " - " + vidBitrates[i]);
+		}
+
+		// ask for bitrate:
+		//const chosenBitrate = prompt('Choose bitrate number (1-' + vidBitrates.length + '): ');
+
+
+		// getting maximum bitrate from all URLs
+		//console.log(vidBitrates);
+
+		// change '<' to '>' to get max. bitrate, if '<' left - download the lowest bitrate file
+		var maxBitrateIndex = vidBitrates.reduce((iMax, x, i, arr) => x < arr[iMax] ? i : iMax, 0); // get highest bitrate video index
+		//console.log("Max bitrate index: " + maxBitrateIndex);
+
+
+		// getting video URLs
+		//console.log("Matches count: " + vidURLs.length);
+		var vidURLtoDownload = vidURLs[maxBitrateIndex].replace(/\\\//g, "\/").slice(8);
+		//console.log(vidURLs[vidURLs.length - 1].replace(/\\\//g, "\\"));
+		console.log("\nDownloading from URL: " + "\x1b[33m%s\x1b[0m", vidURLtoDownload);
+		//console.log(vidURLs);
+
+		//console.log("Downloading file...");
+
+
+		// Downloading a file (creating/downloading)
+		const https = require('https');
+
+		//console.log("Creating file...");
+		var filename = vidTitle + " - " + vidSubtitle + ".[" + Date.now() + "].mp4";
+		console.log("- Created file: " + "\x1b[32m%s\x1b[0m", filename + "\x1b[0m", " in directory " + "\x1b[32m", dirName);
+		console.log("\x1b[32m%s\x1b[0m", "-- Downloading file... (bitrate: " + vidBitrates[maxBitrateIndex] + ")");
+		const file = fs.createWriteStream(dirName + "/" + filename);
+		const request = https.get(vidURLtoDownload, function (response) {
+			response.pipe(file);
+		});
+
+
+
+		//var fileSize = 0;
+		request.on('response', function (data) {
+			var fileSize = parseFloat(data.headers['content-length'] / 1048576).toFixed(2); // 1048576 = 1024 squared, bytes in 1 Megabyte
+			console.log('Total filesize: ' + "\x1b[33m%s\x1b[0m", fileSize + " MB");
+		});
+
+
+
+		/* var downloadedSize;
+		request.on('data', function (chunk) {
+			downloadedSize += chunk.length;
+			console.log('Downloaded ' + (downloadedSize / fileSize.toFixed(2) + ' out of ' + fileSize + " MB"));
+		}); */
+
+
+		// ! that doesn't work ffs
+		// TODO: FIX THAAAAAAAAT
+		request.on('end', function () {
+			console.out("\x1b[32m%s\x1b[0m", 'Downloading finished!');
+		});
+
+		// downloading...
+		//console.log(APIresponse);
+
+	} catch (err) {
+		// Errors in red...~
+		console.log("\x1b[31m%s\x1b[0m", "--- Error as fvk:\n" + err);
 	}
 
-
-	// ask for bitrate:
-	//const chosenBitrate = prompt('Choose bitrate number (1-' + vidBitrates.length + '): ');
-
-
-	// getting maximum bitrate from all URLs
-	//console.log(vidBitrates);
-
-	// change '<' to '>' to get max. bitrate, if '<' left - download the lowest bitrate file
-	var maxBitrateIndex = vidBitrates.reduce((iMax, x, i, arr) => x < arr[iMax] ? i : iMax, 0); // get highest bitrate video index
-	//console.log("Max bitrate index: " + maxBitrateIndex);
-
-
-	// getting video URLs
-	//console.log("Matches count: " + vidURLs.length);
-	var vidURLtoDownload = vidURLs[maxBitrateIndex].replace(/\\\//g, "\/").slice(8);
-	//console.log(vidURLs[vidURLs.length - 1].replace(/\\\//g, "\\"));
-	console.log("\nDownloading from URL: " + "\x1b[33m%s\x1b[0m", vidURLtoDownload);
-	//console.log(vidURLs);
-
-	//console.log("Downloading file...");
-
-
-	// Downloading a file (creating/downloading)
-	const https = require('https');
-
-	//console.log("Creating file...");
-	var filename = vidTitle + " - " + vidSubtitle + ".[" + Date.now() + "].mp4";
-	console.log("- Created file: " + "\x1b[32m%s\x1b[0m", filename + "\x1b[0m", " in directory " + "\x1b[32m", dirName);
-	console.log("\x1b[32m%s\x1b[0m", "-- Downloading file... (bitrate: " + vidBitrates[maxBitrateIndex] + ")");
-	const file = fs.createWriteStream(dirName + "/" + filename);
-	const request = https.get(vidURLtoDownload, function (response) {
-		response.pipe(file);
-	});
-
-	
-
-	//var fileSize = 0;
-	request.on('response', function (data) {
-		var fileSize = parseFloat(data.headers['content-length'] / 1048576).toFixed(2); // 1048576 = 1024 squared, bytes in 1 Megabyte
-		console.log('Total filesize: ' + "\x1b[33m%s\x1b[0m", fileSize + " MB");
-	});
-
-
-
-	/* var downloadedSize;
-	request.on('data', function (chunk) {
-		downloadedSize += chunk.length;
-		console.log('Downloaded ' + (downloadedSize / fileSize.toFixed(2) + ' out of ' + fileSize + " MB"));
-	}); */
-
-
-	// ! that doesn't work ffs
-	// TODO: FIX THAAAAAAAAT
-	request.on('end', function () {
-		console.out("\x1b[32m%s\x1b[0m", 'Downloading finished!');
-	});
-
-	// downloading...
-	//console.log(APIresponse);
-
+	return 1;
 }
 
 function onErr(err) {
